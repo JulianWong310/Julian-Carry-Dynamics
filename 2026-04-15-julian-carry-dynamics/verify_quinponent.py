b860033527f60c329c7756a30d34cf01b68816bb
JulianWong310 <julianwong310@gmail.com>
2026-04-22 15:29:52 -0400

    Format code in verify_quinponent.py

--- 2026-04-15-julian-carry-dynamics/verify_quinponent.py
+++ 2026-04-15-julian-carry-dynamics/verify_quinponent.py
@@ -1,7 +1,7 @@
 def verify_quinponent(n):
     front = 0  # Starting with 5^2
     for i in range(2, n + 1):
-        result = 5**i
+        result = 5 ** i
         print(f"5^{i} = {result} | Leading part is {front}")
         front = front * 5 + 1
 
